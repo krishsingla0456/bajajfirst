4c7d160006b5db607a2044c5e102f2cbc7c64520
krishsingla0456 <krishsingla0456@gmail.com>
2025-02-21 17:42:32 +0530

    index.js

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app
 
     res.json({
       is_success: true,
-      user_id: "sanhita17",
-      email: "sanhita.kundu2020@vitstudent.ac.in",
-      roll_number: "20BEC0215",
+      user_id: "krishsingla0456",
+      email: "krishsingla0456@gmail.com",
+      roll_number: "22BCS11759",
       numbers: numbers,
       alphabets: alphabets,
       highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
